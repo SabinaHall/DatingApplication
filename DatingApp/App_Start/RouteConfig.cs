90935c4384891502f1a72f41f296427e5ce3baf0
Sabina Larsson Hall <Sabinalarssonhall@gmail.com>
2017-12-13 20:25:02 +0100

    Hej! Har lagt till textfält till log in på startsidan. Har ändrat href-länken på knappen för att registrera sig. OBS ingen CSS.

--- DatingApp/App_Start/RouteConfig.cs
+++ DatingApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DatingApp
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Create", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
