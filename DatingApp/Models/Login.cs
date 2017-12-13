f765c696b79ab7ce624f29ddc5cdb5fef11a4d41
Sabina Larsson Hall <Sabinalarssonhall@gmail.com>
2017-12-13 13:37:27 +0100

    hej

--- DatingApp/Models/Login.cs
+++ DatingApp/Models/Login.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,7 @@ namespace DatingApp.Models
 {
     public class Login
     {
-       
+        //[Key]
         public int Id { get; set; }
 
         [Required]
