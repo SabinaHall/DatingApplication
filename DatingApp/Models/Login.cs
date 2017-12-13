dac075646519a49e05231ca7a0925526022c7238
julia <julia@lottingelund.se>
2017-12-13 13:29:04 +0100

    Vy till login

--- DatingApp/Models/Login.cs
+++ DatingApp/Models/Login.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,7 @@ namespace DatingApp.Models
 {
     public class Login
     {
-       
+        [ForeignKey ("Models.Login")]
         public int Id { get; set; }
 
         [Required]
