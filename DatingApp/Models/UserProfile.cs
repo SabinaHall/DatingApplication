c6cc354f0444bc64fbf5af72a6c2fc212182c62a
Sabina Larsson Hall <Sabinalarssonhall@gmail.com>
2017-12-13 10:51:54 +0100

    UserProfile-klassen

---
+++ DatingApp/Models/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatingApp.Models
+{
+    public class UserProfile
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Förnamn")]
+        public string Firstname { get; set; }
+
+        [Required]
+        [Display(Name = "Efternamn")]
+        public string Lastname { get; set; }
+
+        [Required]
+        [Range(18, 99)]
+        [Display(Name = "Ålder")]
+        public int Age { get; set; }
+    }
+}
