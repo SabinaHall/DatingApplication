90580bb27ae32a56fea77b0f0bd9bc46d6df4a23
Filippa Ringqvist <filipparingqvist@gmail.com>
2017-12-13 10:44:03 +0100

    Klass Post och Login

---
+++ DatingApp/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DatingApp.Models
+{
+    public class Post
+    {
+        public int PostId { get; set; }
+        [StringLength(500)]
+        public string Message { get; set; }
+    }
+}
