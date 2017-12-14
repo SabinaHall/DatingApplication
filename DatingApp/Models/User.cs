7e8e2cb5d8ff6ab45ebad1610214b3b9052528d5
Filippa Ringqvist <filipparingqvist@gmail.com>
2017-12-14 11:12:54 +0100

    språk är klart

--- DatingApp/Models/User.cs
+++ DatingApp/Models/User.cs
@@ -4,11 +4,13 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace DatingApp.Models
 {
     public class User
     {
+        
         public int Id { get; set; }
 
         [Required]
